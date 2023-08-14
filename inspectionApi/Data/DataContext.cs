7f9e0c6a486a7c7a3d1576a5792fd50609c27cbe
Alejandro Taichi Suarez <137855619+DevTai22@users.noreply.github.com>
2023-08-14 00:52:09 -0300

    primer commit
    aplicacion InspectionAPIapp que se conecta con InspectionAngular.
    
    asignacion FrontEnd de 1950Labs

---
+++ inspectionApi/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace InspectionAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Inspection> Inspections { get; set; }
+
+        public DbSet<InspectionTypes> InspectionTypes { get; set; }
+
+        public DbSet<Status> Statuses { get; set; }
+    }
+}
---
+++ inspectionApi/Models/InspectionTypes.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InspectionAPI
+{
+    public class InspectionTypes
+    {
+        public int Id { get; set; }
+
+        [StringLength(20)]
+        public string InspectionName { get; set; } = string.Empty;
+    }
+}
